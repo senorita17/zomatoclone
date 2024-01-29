71dbe11ef6f0454fded0d8fa96d8006e560ce9cd
senorita17 <145017259+senorita17@users.noreply.github.com>
2024-01-29 18:43:21 +0530

    Add files via upload

---
+++ zom.js
@@ -0,0 +1,7 @@
+document.getElementById('shareAppLinkBtn').addEventListener('click', function() {
+    // Get the value of the email input
+    var emailInputValue = document.getElementById('emailInput').value;
+
+    // Log a message to the console
+    console.log('Sharing app link with email:', emailInputValue);
+  });
